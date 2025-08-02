9c7206f2fd69e3aa2aa7b2559be406746e0cffa1
rob <heidlessemail14@gmail.com>
2025-08-02 22:15:54 +0000

    theme selector: light/dark selector not working

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -124,7 +124,7 @@ export function ThemeProvider({
 
   const value = {
     mode,
-    setMode,
+    setMode: setMode as (mode: ThemeMode) => void,
     colorTheme,
     setColorTheme,
     toggleMode,
